20a6fb46eed38156f0e3eae75bf39ae01b1e14d7
Bloople <galloc1@outlook.com>
2022-11-21 11:23:42 +1030

    Added: -Incomplete player controls

--- untitled train sabotage game/Assets/Scripts/CameraController.cs
+++ untitled train sabotage game/Assets/Scripts/CameraController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
-public class PlayerCamera : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     public Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,6 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        
     }
 }
--- untitled train sabotage game/Assets/Scripts/PlayerController.cs
+++ untitled train sabotage game/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,20 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
+    public float pitch;
+    public float yaw;
+    public float xaxis;
+    public float yaxis;
+    public float mouseSensitivity;
+
+    //Rigidbody attached to the player
     Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +24,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Rotate();
+    }
+    private void Move()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            rb.velocity = transform.forward*3;
+            rb.velocity = transform.forward * 3;
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -transform.right * 3;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = -transform.forward * 3;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * 3;
+        }
+    }
+    public void Rotate()
+    {
+        //stores initial pitch, yaw, and roll of the player
+        pitch = transform.eulerAngles.x;
+        yaw = transform.eulerAngles.y;
+        float roll = 0;
+
+        xaxis = Input.GetAxis("Mouse X");
+        yaxis = Input.GetAxis("Mouse Y");
+        //adds mouse movement to pitch and yaw
+        yaw +=Input.GetAxis("Mouse X")*mouseSensitivity;
+        pitch+=Input.GetAxis("Mouse Y")*mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
