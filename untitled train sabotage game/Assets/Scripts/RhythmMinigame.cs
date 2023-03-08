e4ac7b5aad415d59e5b6f5637bc5a1e62ee4ddb8
AIE\s215843 <s215843@students.aie.edu.au>
2023-03-08 20:25:05 +1030

    Good work on visual representation of the rhythm minigame.

--- untitled train sabotage game/Assets/Scripts/RhythmMinigame.cs
+++ untitled train sabotage game/Assets/Scripts/RhythmMinigame.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class RhythmMinigame : MonoBehaviour
 {
     //Stores the starting position of each element of the canvas (used to reset their positions)
-    private List<Vector3> canvasElementsStartingPositions;
+    private List<Vector3> carriageSpriteStartingPositions;
+    //Stores the prefab instance that contains each carriage sprite
+    private List<Transform> carriageSprites;
 
 
     // Start is called before the first frame update
     void Start()
     {
         //Gets the starting position of each canvas element and stores them in a list
-        canvasElementsStartingPositions = new List<Vector3>();
-        for (int i = 0; i < transform.childCount; i++)
+        carriageSpriteStartingPositions = new List<Vector3>();
+        for (int i = 0; i < 5; i++)
+        {
+            Transform child = transform.GetChild(i).GetChild(0);
+            carriageSpriteStartingPositions.Add(child.GetComponent<RectTransform>().anchoredPosition);
+        }
+
+        //Gets a list of carriage prefab instances (grants access to the carriage sprites)
+        carriageSprites = new List<Transform>();
+        for (int i=0; i < 5; i++)
         {
             Transform child = transform.GetChild(i);
-            canvasElementsStartingPositions.Add(child.GetComponent<RectTransform>().anchoredPosition);
+            carriageSprites.Add(child);
         }
     }
 
@@ -24,10 +34,10 @@ public class RhythmMinigame : MonoBehaviour
     void Initialise()
     {
         //Resets the position of each element on the canvas
-        for(int i= 0; i < canvasElementsStartingPositions.Count; i++)
+        for(int i= 0; i < carriageSpriteStartingPositions.Count; i++)
         {
             Transform child = transform.GetChild(i);
-            child.GetComponent<RectTransform>().anchoredPosition = canvasElementsStartingPositions[i];
+            child.GetComponent<RectTransform>().anchoredPosition = carriageSpriteStartingPositions[i];
         }
     }
 
